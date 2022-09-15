b5251768dc6e62ba8251ebb3a2fef8a4931ece74
reno <seth.rasmussen13@gmail.com>
2022-09-15 17:00:11 -0400

    updates script to create new notes in obsidian

--- newtest.sh
+++ newtest.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-read -p 'New Client Name: ' testvar
+RED=$'\033[0;31m'
+NC=$'\033[0m'
+read -p 'New Client Name: ' cname
+cap="$(tr '[:lower:]' '[:upper:]' <<< ${cname:0:1})${cname:1}"
 cd ~/Documents/engagements/
-mkdir -p $testvar/{ext/{forced_auth,harvest,nmap},int,prereqs,reciepts,reports/{deliverables/nessus,final,initial},screenshots,se}
-echo 'New testing directory created at ~/Documents/engagements/' $testvar
+mkdir -p $cname/{ext/{forced_auth,harvest,nmap},int,prereqs,reciepts,reports/{deliverables/nessus,final,initial},screenshots,se}
+echo "${RED}[!!!]${NC} New testing directory created at ~/Documents/engagements/"$cname"."
+cd ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/0xreno\ Notes/Work/
+cp Template.md $cap.md
+sed -i '' -e "s/Client/$cap/g" "$cap.md"
+echo "${RED}[!!!]${NC} A new note has been created in Obsidian."
