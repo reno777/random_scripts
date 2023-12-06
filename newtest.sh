1b9934b734b6fb3b964aef6ca82c6693e6d7daff
reno <seth.rasmussen13@gmail.com>
2023-12-06 13:26:12 -0500

    Adds password spray list to new directory

--- newtest.sh
+++ newtest.sh
@@ -5,7 +5,8 @@ NC=$'\033[0m'
 read -p 'New Client Name: ' cname
 cap="$(tr '[:lower:]' '[:upper:]' <<< ${cname:0:1})${cname:1}"
 cd ~/Documents/engagements/
-mkdir -p $cname/{ext/{forced_auth,harvest,nmap},int,prereqs,reciepts,reports/{deliverables/nessus,final,initial},screenshots,se}
+mkdir -p $cname/{ext/{forced_auth,harvest,nmap,pwspray},int,prereqs,reciepts,reports/{deliverables/nessus,final,initial},screenshots,se}
+cp ~/Documents/engagements/resources/passwords-all.txt $cname/ext/pwspray/.
 echo "${RED}[!!!]${NC} New testing directory created at ~/Documents/engagements/"$cname"."
 cd ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/0xreno\ Notes/Work/
 cp Template.md $cap.md
